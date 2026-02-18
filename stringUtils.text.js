3184c70ddc860a0a8a6d0e148ccc392ee90d08f0
Dbh <158257326+Dishabh13@users.noreply.github.com>
2026-02-18 10:48:33 +0530

    Update stringUtils.text.js

--- stringUtils.text.js
+++ stringUtils.text.js
@@ -7,3 +7,7 @@
    test('reverse string', () => {
        expect(reverse('hello')).toBe('olleh');
    });
+test('legacy check', () => {
+    console.log("Tests passed!");
+    expect(true).toBe(true);
+});
