aa07956abfd089d69873ad5a007a02826b5b8482
Dbh <158257326+Dishabh13@users.noreply.github.com>
2026-02-18 10:30:27 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -6,8 +6,10 @@ def test_add():
    assert add(-1, 1) == 0
    
 def test_subtract():
-   assert subtract(5, 3) == -2
+   assert subtract(5, 3) == 2
    assert subtract(0, 5) == -5
+def test_square():
+   assert square(2)==4
    
 def test_multiply():
    assert multiply(3, 4) == 12
